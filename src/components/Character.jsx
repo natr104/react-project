819fa902ff2b6711f33799193fef0ec51d62f0c6
Nathan <troy.nathan@gmail.com>
2022-04-12 12:58:36 +1000

    started creating components

---
+++ src/components/Character.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Character() {
+    
+    return (
+        <div>
+            <h2>Character</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem fuga, laudantium id temporibus repellendus asperiores esse molestias, culpa quis amet sequi magni sint? Distinctio delectus aspernatur quidem fugiat labore sequi.</p>
+        </div>
+    )
+}
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Home() {
+
+    return (
+        <div>
+            <h2>Home</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime aspernatur quisquam qui placeat animi quae temporibus voluptates voluptas exercitationem eum veniam accusantium, eligendi amet illum adipisci optio nulla. Illo, voluptatibus.</p>   
+        </div>
+    )
+}
