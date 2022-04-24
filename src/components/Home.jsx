a12f8189ae91b7a6b5810678a03b587bb54e9cf9
natr104 <troy.nathan@gmail.com>
2022-04-24 14:08:28 +1000

    cleaning up unused code

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,11 @@
 import React from "react"
-import Navbar from "./Navbar"
-import { Outlet } from "react-router-dom"
-
-
-
 
 export default function Home() {
 
     return (
         <div>
             <h2>Home</h2>
-            
+            <p>An application to look up World of Warcraft character progression in raids and the current season of Mythic+. Created by Nathan Troy aka Kirasta of {'<Immaterial>'}, Saurfang (OCE).</p>
         </div>
     )
 }
--- src/utilities/utilities.js
+++ src/utilities/utilities.js
@@ -1,13 +1,9 @@
-import { useContext } from "react";
-import { SavedCharactersContext } from "../contexts/SavedCharactersContext";
-
 export const fetchCharacter = async ({characterName, realm}) => {
     const charURL = `https://raider.io/api/v1/characters/profile?region=us&realm=${realm}&name=${characterName}&fields=guild%2Ccovenant%2Cgear%2Craid_progression%2Cmythic_plus_scores_by_season:current`;
     console.log(characterName, realm);
     try {
         const response = await fetch(charURL);
         const character = await response.json();
-        console.log(character);
         if (character.statusCode===400) {
             alert(`${character.message}`)
             return null;
@@ -23,7 +19,6 @@ export const fetchCharacter = async ({characterName, realm}) => {
 export const handleSubmit = async (event, character, characterDetails, setCharacterDetails)=> {
     event.preventDefault();
     const returnedCharacter = await fetchCharacter(character);
-    console.log(returnedCharacter);
     return setCharacterDetails(returnedCharacter);
 }
 
